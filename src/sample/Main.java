5dbd5b0fba02ed901db3fbd1f89c80e0fbf036fa
Mike Fowler <fowler_mychale@yahoo.com>
2019-01-22 20:57:23 -0800

    Table_v3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,16 +15,27 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("ShopAtHome Dashboard");
+        Image image = new Image("https://raw.githubusercontent.com/fowler-mychale/tableView_v1/master/src/sample/css/sah-icon.png");
+        primaryStage.getIcons().add(image);
         primaryStage.setScene(new Scene(root, 1200, 575));
+
+
+
+
         primaryStage.show();
 
-   new FadeIn(root).play();
+
+
+
+
+        new FadeIn(root).play();
     }
 
 
