352f0769b0c1d1a128ba45d537e88f7acd77ff5c
Mike Fowler <fowler_mychale@yahoo.com>
2019-05-29 18:23:31 -0700

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,11 @@
 package sample;
 
-
-
 import animatefx.animation.FadeIn;
 import javafx.application.Application;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
-
 import javafx.stage.Stage;
-
 import javafx.scene.image.Image;
 
 
